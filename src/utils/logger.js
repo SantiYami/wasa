b0c017f5fd40e4045f2feecca72331181e9b8134
SantiYami <davidgarces.98@gmail.com>
2026-08-23 20:04:06 -0500

    fix: set Windows console to UTF-8 code page (65001) in logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,16 @@
+import { execSync } from 'child_process';
 import pino from 'pino';
 import pinoPretty from 'pino-pretty';
 import { config } from '../config/index.js';
+
+// En Windows, configurar la consola para usar UTF-8 (Code Page 65001) para que los acentos no se corrompan
+if (process.platform === 'win32') {
+  try {
+    execSync('chcp 65001', { stdio: 'ignore' });
+  } catch {
+    // Silencioso en caso de entornos con permisos restringidos
+  }
+}
 
 const stream = pinoPretty({
   colorize: true,
